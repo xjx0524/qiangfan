c2cb135c1f88aa80c19d47d2e584ddf481b27ff3
xjx0524 <xjx524@gmail.com>
2014-07-17 18:10:39 +0800

    能单人手动抢了。。。

--- goods.php
+++ goods.php
@@ -7,7 +7,7 @@ class Goods {
 
 	public static function getGoods() {
 		$ch = curl_init();
-		curl_setopt($ch, CURLOPT_URL, Goods::goodsUrl);
+		curl_setopt($ch, CURLOPT_URL, Goods::$goodsUrl);
 		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 		$res = json_decode(curl_exec($ch));
 		curl_close($ch);
@@ -20,5 +20,56 @@ class Goods {
 			}
 		}
 		return $list;
+	}
+
+	public static function buy() {
+		$couponId = '3953839';
+		$vmCode = '0231003';
+		$vTypeId = '2';
+		$sellerId = 2;
+		$tabCategoryId = 2;
+		$willList = [5006, 7249];
+		$productList = Goods::getGoods();
+
+		$options = [
+			CURLOPT_URL => Goods::$buyUrl,
+			CURLOPT_HEADER => true,
+			CURLOPT_RETURNTRANSFER => true,
+			CURLOPT_POST => true,
+			CURLOPT_COOKIE => "PHPSESSID=5usi2792b1hcsdcvjabbfb5356; SHOPPINGCART=cyd9X7Lea8OWke226nExPQER7ODeqAhP8HBwPEWSJlOwA99SzCKVB3z61Nag5-KyjLFHXuV0KwGPPlO5DszBVnu3wFwf_pTIvQZzjwdMjXXBbpEs0V9y4FLRnUf07fgaKaT03Ms5ZWPnNboKVhjRzD2yt-9TL4y135Uy0J1WmwM; Hm_lvt_f8cb16b7a685768c976896e565ad32ae=1405560087; Hm_lpvt_f8cb16b7a685768c976896e565ad32ae=1405562211"
+		];
+		foreach ($willList as $productId) {
+			if ($p = Goods::get($productList, $productId)) {
+				$data = json_encode([
+					"sellerId" => $sellerId,
+					"couponId" => $couponId,
+					"productId" => $productId,
+					"vmCode" => $vmCode,
+					"vTypeId" => $vTypeId,
+					"tabCategoryId" => $tabCategoryId
+				]);
+				$options[CURLOPT_POSTFIELDS] = $data;
+				$ch = curl_init();
+				curl_setopt_array($ch, $options);
+				$response = curl_exec($ch);
+				if (curl_getinfo($ch, CURLINFO_HTTP_CODE) == '200') {
+					$headerSize = curl_getinfo($ch, CURLINFO_HEADER_SIZE);
+					$header = substr($response, 0, $headerSize);
+					$body = substr($response, $headerSize);
+				}
+				curl_close($ch);
+				echo $body;
+				break;
+			}
+		}
+
+	}
+
+	private static function get($productList, $productId) {
+		foreach ($productList as $p) {
+			if ($p->productId == $productId)
+				return $p;
+		}
+		return null;
 	}
 }
---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+header("Content-type: text/html; charset=utf-8");
+require_once('goods.php');
+
+//Goods::buy();
+var_dump(Goods::getGoods());
+//foreach (Goods::getGoods() as $value) {
+//	$pic = $value->picMap[0];
+//	echo "<p>
+//	<img src='$pic'/>
+//	$value->fullName
+//	</p>";
+//}
